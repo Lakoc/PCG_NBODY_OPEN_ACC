551ef5e62fe6c3c12d67223ced1d7ce278183fcb
Lakoc <xpolok03@stud.fit.vutbr.cz>
2022-11-30 22:46:53 +0100

    initial commit

---
+++ step1/nbody.cpp
@@ -0,0 +1,100 @@
+/**
+ * @file      nbody.cpp
+ *
+ * @author    Jiri Jaros \n
+ *            Faculty of Information Technology \n
+ *            Brno University of Technology \n
+ *            jarosjir@fit.vutbr.cz
+ *
+ * @brief     PCG Assignment 2
+ *            N-Body simulation in ACC
+ *
+ * @version   2022
+ *
+ * @date      11 November  2020, 11:22 (created) \n
+ * @date      15 November  2022, 14:10 (revised) \n
+ *
+ */
+
+#include <math.h>
+#include <cfloat>
+#include "nbody.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                       Declare following structs / classes                                          //
+//                                  If necessary, add your own classes / routines                                     //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// Compute gravitation velocity
+void calculate_gravitation_velocity(const Particles& p,
+                                    Velocities&      tmp_vel,
+                                    const int        N,
+                                    const float      dt)
+{
+
+
+
+}// end of calculate_gravitation_velocity
+//----------------------------------------------------------------------------------------------------------------------
+
+void calculate_collision_velocity(const Particles& p,
+                                  Velocities&      tmp_vel,
+                                  const int        N,
+                                  const float      dt)
+{
+
+
+
+}// end of calculate_collision_velocity
+//----------------------------------------------------------------------------------------------------------------------
+
+/// Update particle position
+void update_particle(const Particles& p,
+                     Velocities&      tmp_vel,
+                     const int        N,
+                     const float      dt)
+{
+
+
+
+}// end of update_particle
+//----------------------------------------------------------------------------------------------------------------------
+
+
+
+/// Compute center of gravity
+float4 centerOfMassGPU(const Particles& p,
+                       const int        N)
+{
+
+  return {0.0f, 0.0f, 0.0f, 0.0f};
+}// end of centerOfMassGPU
+//----------------------------------------------------------------------------------------------------------------------
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// Compute center of mass on CPU
+float4 centerOfMassCPU(MemDesc& memDesc)
+{
+  float4 com = {0 ,0, 0, 0};
+
+  for(int i = 0; i < memDesc.getDataSize(); i++)
+  {
+    // Calculate the vector on the line connecting points and most recent position of center-of-mass
+    const float dx = memDesc.getPosX(i) - com.x;
+    const float dy = memDesc.getPosY(i) - com.y;
+    const float dz = memDesc.getPosZ(i) - com.z;
+
+    // Calculate weight ratio only if at least one particle isn't massless
+    const float dw = ((memDesc.getWeight(i) + com.w) > 0.0f)
+                          ? ( memDesc.getWeight(i) / (memDesc.getWeight(i) + com.w)) : 0.0f;
+
+    // Update position and weight of the center-of-mass according to the weight ration and vector
+    com.x += dx * dw;
+    com.y += dy * dw;
+    com.z += dz * dw;
+    com.w += memDesc.getWeight(i);
+  }
+  return com;
+}// end of centerOfMassCPU
+//----------------------------------------------------------------------------------------------------------------------
